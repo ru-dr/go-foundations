4808f74b4fa643ea2cc5b2a861a5dd9c3d4da903
ru-dr <ping@rudr.me>
2026-08-27 23:35:17 -0400

    day-6: errors as values, custom error type, Book.Validate

--- day6/main.go
+++ day6/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Book struct {
 	title  string
@@ -17,11 +19,24 @@ func (e *ValidationError) Error() string {
 	return fmt.Sprintf("invalid %s : %v", e.Field, e.Value)
 }
 
-func (b Book) Validate() error 	{
-	
+func (b Book) Validate() error {
+	if b.pages <= 0 {
+		return &ValidationError{"pages", b.pages}
+	}
+	if b.title == "" {
+		return &ValidationError{"title", b.title}
+	}
+	return nil
+}
+
+func (b Book) String() string {
+	if err := b.Validate(); err != nil {
+		return fmt.Sprintf("Book(invalid): %v", err)
+	}
+	return fmt.Sprintf("%s by %s, %d pages", b.title, b.author, b.pages)
 }
 
 func main() {
-	b := Book{title: "", author: "Orwell", pages: 300}
+	b := Book{title: "", author: "Orwell", pages: 0}
 	fmt.Println(b)
 }
